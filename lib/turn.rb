6bc4c93d01b93732bc1941995f3855e7c94d5515
essential-blog-3393 <jcpleitez@outlook.com>
2020-09-21 17:57:37 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -19,7 +19,29 @@ def input_to_index(user_input)
   user_input.to_i - 1
   end
   
-  
+  def valid_move?(board, index)
+  def position_taken?(array, ind)
+    if array[ind] == " " || array[ind] == "" || array[ind] == nil
+      return false
+    else
+      return true
+    end
+  end
+
+  def on_board?(num)
+    if num.between?(0, 8) == true
+      return true
+    else
+      return false
+    end
+  end
+
+  if (position_taken?(board, index)) == false && (on_board?(index) == true)
+    return true
+  else
+    return false
+  end
+end
   
   def move(board, user_input, character = "X")
   board[user_input] = character
