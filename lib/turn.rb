65d62f68a6b74e8d8f6df08b4102352db4c2dfe7
essential-blog-3393 <jcpleitez@outlook.com>
2020-09-21 18:08:40 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -47,3 +47,14 @@ end
   board[user_input] = character
 end
 
+def turn (board)
+  puts "Please enter 1-9:"
+  user_input = gets.chomp
+  index = input_to_index(user_input)
+  if valid_move?(board, index) == true
+    move(board, index)
+    display_board(board)
+  else
+    turn(board)
+  end
+end
